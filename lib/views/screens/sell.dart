47187a01a59b2f52e9bdb8b7c4e2831ac11cb3ed
Wasiul <sakif049@gmail.com>
2025-12-23 12:53:56 +0600

    add: sell unit

--- lib/views/screens/sell.dart
+++ lib/views/screens/sell.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:j4corp/controllers/unit_controller.dart';
+import 'package:j4corp/models/unit.dart';
+import 'package:j4corp/utils/custom_snackbar.dart';
 import 'package:j4corp/views/base/custom_app_bar.dart';
 import 'package:j4corp/views/base/custom_button.dart';
-import 'package:j4corp/views/base/custom_drop_down.dart';
+import 'package:j4corp/views/base/custom_text_field.dart';
+import 'package:j4corp/views/base/unit_drop_down.dart';
+import 'package:j4corp/views/base/vehicle_card.dart';
 
 class Sell extends StatefulWidget {
   const Sell({super.key});
@@ -11,7 +17,24 @@ class Sell extends StatefulWidget {
 }
 
 class _SellState extends State<Sell> {
-  String? selected;
+  final unit = Get.find<UnitController>();
+  final info = TextEditingController();
+
+  Unit? selected;
+
+  void onSubmit() async {
+    final message = await unit.sellUnit(selected!.id, info.text);
+
+    if (message == "success") {
+      setState(() {
+        selected = null;
+        info.clear();
+      });
+      customSnackbar("Your unit is up to sell!", isError: false);
+    } else {
+      customSnackbar(message);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,26 +49,33 @@ class _SellState extends State<Sell> {
               spacing: 16,
               children: [
                 const SizedBox(),
-                CustomDropDown(
+                UnitDropDown(
                   onChanged: (val) {
                     setState(() {
-                      selected = [
-                              "BMW Motorcycles of San Antonio",
-                              "BMG Xtreme Sports",
-                              "Triumph Houston",
-                            ][val];
+                      selected = val;
                     });
                   },
-                  options: [
-                    "Kawasaki-KLX110R",
-                    "Kawasaki-KLX110R",
-                    "Kawasaki-KLX110R",
-                  ],
-                  title: "Select Unit",
+                  hintText: "Select a unit to sell",
                 ),
-                
-                // if (selected != null) VehicleCard(),
-                if (selected != null) CustomButton(text: "Submit"),
+
+                if (selected != null) VehicleCard(selected!),
+
+                if (selected != null)
+                  CustomTextField(
+                    title: "Additional Information",
+                    controller: info,
+                    hintText: "Say something about this product",
+                    lines: 5,
+                  ),
+                if (selected != null)
+                  Obx(
+                    () => CustomButton(
+                      onTap: onSubmit,
+                      text: "Submit",
+                      isLoading: unit.isLoading.value,
+                    ),
+                  ),
+                const SizedBox(height: 20),
               ],
             ),
           ),
